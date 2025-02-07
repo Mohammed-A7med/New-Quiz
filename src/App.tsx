3d4795ecd196e5e174de36b042afe2a9b976cc8c
Mohamed Ahmed <mohamed.ahmed@example.com>
2025-02-07 18:21:29 +0200

    Implement question selection and answer tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Quiz from './Components/Quiz/Quiz'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Quiz/>
     </>
   )
 }
